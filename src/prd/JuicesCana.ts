96817b07f3f5b019ec806ca4e973e1dd2a6aa686
Ismael-g5 <ismaelguedessilva@gmail.com>
2022-01-31 21:34:30 -0300

    atualização1

--- src/prd/JuicesCana.ts
+++ src/prd/JuicesCana.ts
@@ -1,7 +1,8 @@
 type Jitems  = {name: string; amount: number; price: number };
-
+type orderStatus = 'open' | 'closed'
 export class JuicesCana {
     private readonly _items: Jitems[] = [];
+    private _orderStatus: orderStatus = 'open';
 
     addItem(item: Jitems): void {
         this._items.push(item);
@@ -15,11 +16,39 @@ export class JuicesCana {
         return this._items;
 
     }
+    get orderStatus(): orderStatus {
+        return this._orderStatus;
+
+    }
     totalPrice(): number {
         return this._items.reduce((total, next) => total + next.price, 0)
     }
     totalAmount(): number {
         return this._items.reduce((total, next) => total + next.amount, 0)
+    }
+    orderCheck(): void {
+        if(this.isEmpty()) {
+            console.log('Realize seu Pedido');
+            return;
+        }
+        this._orderStatus = 'closed';
+        this.sendMessage('seu pedido foi recebido!');
+        this.saveOrder();
+
+    }
+
+    isEmpty(): boolean {
+        return this._items.length === 0;
+    }
+    sendMessage(msg: string): void{
+        console.log(`seu pedido com o total de R$${this.totalPrice()} foi recebido!`);
+    }
+    saveOrder(): void {
+        console.log('Seu pedido esta sendo produzido!')
+    }
+
+    clear(): void {
+        this._items.length = 0;
     }
 }
 
@@ -32,3 +61,5 @@ console.log(juicesCana.items);
 console.log(juicesCana.items.length);
 console.log(juicesCana.totalAmount());
 console.log(juicesCana.totalPrice());
+juicesCana.orderCheck();
+console.log(juicesCana.orderStatus);
